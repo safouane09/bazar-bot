18c79f0fe20497d3ac0f98e15c08cccf75af13c4
safouane09 <43318440+safouane09@users.noreply.github.com>
2025-03-19 16:41:33 +0100

    Update config.py

--- config.py
+++ config.py
@@ -5,7 +5,7 @@ from dotenv import load_dotenv
 load_dotenv()
 
 load_dotenv()
-
+GDRIVE_FOLDER_ID = "126XootI_okGOiiwgL-8ZxHHtXs1LaKgE"  # Replace with your folder ID
 BOT_TOKEN = os.getenv("BOT_TOKEN")
 ADMIN_ID = [814180858]  # Keep as a list
   # Remove the list brackets
